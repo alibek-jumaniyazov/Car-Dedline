55ee26996e4e246653d395f2b7b2d738a59b4f57
alibek-jumaniyazov <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-12-19 13:30:54 +0500

    add

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/home-header/Header'
+
+export default function Home() {
+  return (
+    <div className='Home'>
+        <div className="home-header">
+            <Header/>
+        </div>
+        
+    </div>
+  )
+}
