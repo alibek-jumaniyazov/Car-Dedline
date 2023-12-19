0ab076eb82ba509a7701bb7dc30db02e0a4b763e
alibek-jumaniyazov <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-12-19 21:55:40 +0500

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/style.css'
+import './styles/header.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
