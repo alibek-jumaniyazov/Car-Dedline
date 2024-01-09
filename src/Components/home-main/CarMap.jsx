b4d66b548011f94166ef0539431bf1d166713208
any amir <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-01-09 04:00:26 -1200

    add

---
+++ src/Components/home-main/CarMap.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function CarMap() {
+    return (
+        <div className='CarMap'>
+            <h1 className="car-maps__title">Что включает программа</h1>
+            <div className="car-maps">
+                <div className="one-map">
+                    <h1>01</h1>
+                    <img src="" alt="" />
+                    <div className="reverse-texts">
+                        <p>АРЕНДА СПОРТКАРА</p>
+                        <hr />
+                        <span> нагрузкам </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
